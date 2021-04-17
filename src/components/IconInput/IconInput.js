9fec9e2be264c581250266cfe705687ac766d38f
Alvaro Alvarez <alv.ale.alvarez@gmail.com>
2021-04-17 14:27:27 -0400

    select and inputIcon completed

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,92 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const SIZES = {
+  small: {
+    fontSize: 14,
+    height: 24,
+    border: 1,
+    iconSize: 16,
+    stroke: 1,
+  },
+  large: {
+    fontSize: 18,
+    height: 36,
+    border: 2,
+    iconSize: 24,
+    stroke: 2,
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const styles = SIZES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to Progress Bar: ${size}`);
+  }
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--iconSize": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} strokeWidth={styles.stroke} />
+      </IconWrapper>
+      <TextInput
+        label={label}
+        placeholder={placeholder}
+        style={{
+          "--height": styles.height / 16 + "rem",
+          "--width": width + "px",
+          "--fontSize": styles.fontSize / 16 + "rem",
+          "--border": styles.border + "px",
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+}
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--iconSize);
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+
+  color: inherit;
+  font-size: var(--fontSize);
+  font-weight: 700;
+
+  padding-left: var(--height);
+
+  border: none;
+  border-bottom: var(--border) solid ${COLORS.black};
+  outline-offset: 2px;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,70 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
+  console.log(displayedValue);
+
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <Presentational>
+        <span>{displayedValue}</span>
+        <Icon id="chevron-down" strokeWidth={2} size={24} />
+      </Presentational>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const Presentational = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray500};
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 1rem;
+  font-family: "Roboto";
+  font-weight: 400;
+
+  border-radius: 8px;
+
+  padding: 12px 16px;
+
+  span {
+    padding-right: 24px;
+  }
+  
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
